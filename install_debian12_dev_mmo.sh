dc74a8f16cf66f65cb7e8fc024c3c7ae46372f69
MarlonMobioh <166839977+MarlonMobioh@users.noreply.github.com>
2024-04-12 18:09:47 +0200

    Update install_debian12_dev_mmo.sh

--- install_debian12_dev_mmo.sh
+++ install_debian12_dev_mmo.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# Marlon MOBIOH
-# 12/04/2024
-# Télécharger le fichier = wget -P /tmp <permalink>
-# Executer le fichier git = bash install_debian12_dev_mmo.sh
-# Voir en commentaire le détail de chaque commande
-
 # Vérifier si l'utilisateur courant est root
 if [ "$(id -u)" -ne 0 ]; then
     echo "Ce script doit être exécuté en tant que root"
