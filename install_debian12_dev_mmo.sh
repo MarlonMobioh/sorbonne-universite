7132f023610378d9a9e9e3dfa9f6af61a49f78ec
MarlonMobioh <166839977+MarlonMobioh@users.noreply.github.com>
2024-04-12 18:18:18 +0200

    Update install_debian12_dev_mmo.sh

--- install_debian12_dev_mmo.sh
+++ install_debian12_dev_mmo.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Marlon MOBIOH
+# 12/04/2024
+# Télécharger le fichier = wget -P /tmp <permalink>
+# Executer le fichier git = bash install_debian12_dev_mmo.sh
+# Voir en commentaire le détail de chaque commande
+
 # Vérifier si l'utilisateur courant est root
 if [ "$(id -u)" -ne 0 ]; then
     echo "Ce script doit être exécuté en tant que root"
@@ -54,3 +60,30 @@ for user_home in /home/*/; do
         echo "Le shell de l'utilisateur $username a été modifié en /usr/bin/bash"
     fi
 done
+
+# Ajouter la configuration de firewall
+firewall_config='<?xml version="1.0" encoding="utf-8"?>
+<zone>
+  <short>Work</short>
+  <description>For use in work areas. You mostly trust the other computers on networks to not harm your computer. Only selected incoming connections are accepted.</description>
+  <service name="ssh"/>
+  <service name="http"/>
+  <service name="https"/>
+  <source address="172.22.0.0/24"/>
+  <source address="10.50.0.0/18"/>
+  <source address="134.157.134.0/24"/>
+  <source address="10.11.20.0/22"/>
+  <source address="134.157.142.0/23"/>
+  <source address="134.157.1.240/23"/>
+  <source address="134.157.143.0/24"/>
+  <source address="10.11.7.239"/>
+  <source address="134.157.23.239"/>
+  <forward/>
+</zone>'
+
+echo "$firewall_config" > /etc/firewalld/zones/work.xml
+echo "Configuration de firewall ajoutée dans work.xml."
+
+# Redémarrer le service firewalld pour appliquer les modifications
+systemctl restart firewalld
+echo "Le service firewalld a été redémarré."
