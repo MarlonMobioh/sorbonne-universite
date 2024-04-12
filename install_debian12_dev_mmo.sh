36a725b113bb692459518dae8275223742b39443
MarlonMobioh <166839977+MarlonMobioh@users.noreply.github.com>
2024-04-12 17:57:45 +0200

    Update install_debian12_dev_mmo.sh

--- install_debian12_dev_mmo.sh
+++ install_debian12_dev_mmo.sh
@@ -1,10 +1,10 @@
+#!/bin/bash
+
 # Marlon MOBIOH
 # 12/04/2024
 # Télécharger le fichier = wget -P /tmp <permalink>
 # Executer le fichier git = bash install_debian12_dev_mmo.sh
 # Voir en commentaire le détail de chaque commande
-
-#!/bin/bash
 
 # Vérifier si l'utilisateur courant est root
 if [ "$(id -u)" -ne 0 ]; then
