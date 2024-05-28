b8f96f38b49735146c0df908628cc8f62031a3c2
MarlonMobioh <166839977+MarlonMobioh@users.noreply.github.com>
2024-05-28 17:50:23 +0200

    Update install_redhat8-9_dev_mmo.sh

--- install_redhat8-9_dev_mmo.sh
+++ install_redhat8-9_dev_mmo.sh
@@ -292,9 +292,13 @@ systemctl list-units --type=service
 
 # Enregistrement dans RedHat (mmobioh ; Sorbonne@2023)
 echo "Enregistrement dans RedHat :"
+echo "Nettoyage des informations d'inscription précédentes"
 subscription-manager clean
+echo "Enregistrement de l'utilisateur [mmobioh]"
 subscription-manager register --username=mmobioh --password=Sorbonne@2023
+echo "Affichage de la liste des abonnements disponibles"
 subscription-manager list --available
+echo "Attachement de l'abonnement spécifique identifié par le code de pool [8a85f99977b0c0420177f2a086211111s]"
 subscription-manager attach --pool=8a85f99977b0c0420177f2a086211111
 
 # Vidage du contenu des fichiers de journalisation système
