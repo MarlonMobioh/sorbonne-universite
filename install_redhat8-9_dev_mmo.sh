911da36d3806081fd19bc9e0edd427c00ab56753
MarlonMobioh <166839977+MarlonMobioh@users.noreply.github.com>
2024-05-28 17:44:23 +0200

    Update install_redhat8-9_dev_mmo.sh

--- install_redhat8-9_dev_mmo.sh
+++ install_redhat8-9_dev_mmo.sh
@@ -66,11 +66,16 @@ machine_ip=$(hostname -I | awk '{print $1}')
 # Nom d'hôte à associer
 machine_hostname=$(hostname)
 
-# Vérifier si l'adresse IP et le nom d'hôte sont définis dans /etc/hosts
-if [[ -z "$machine_ip" || -z "$machine_hostname" ]]; then
-    echo "Impossible de récupérer l'adresse IP ou le nom d'hôte. Arrêt du script."
-    exit 1
+# Vérifier que l'adresse IP et le nom d'hôte sont correctement définis dans /etc/hosts
+if grep -q "$machine_ip\s*$machine_hostname" /etc/hosts; then
+    echo "L'adresse IP et le nom d'hôte sont correctement définis dans /etc/hosts."
+else
+    echo "L'adresse IP et/ou le nom d'hôte ne sont pas correctement définis dans /etc/hosts."
+    echo "Ajout des entrées dans /etc/hosts..."
+    echo "$machine_ip $machine_hostname" >> /etc/hosts
+    echo "Les entrées ont été ajoutées à /etc/hosts."
 fi
+
 
 # Fonction pour vérifier l'appartenance d'un utilisateur au groupe wheel (équivalent de sudo)
 user_in_wheel_group() {
