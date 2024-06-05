47a88044cc70adca34410fcd0026e3ff84a25376
MarlonMobioh <166839977+MarlonMobioh@users.noreply.github.com>
2024-06-05 17:31:24 +0200

    Update install_debian11-12_preprod_mmo.sh

--- install_debian11-12_preprod_mmo.sh
+++ install_debian11-12_preprod_mmo.sh
@@ -242,7 +242,7 @@ sleep 3
 ntp1="134.157.33.254"
 
 # Modifier le fichier /etc/systemd/timesyncd.conf avec l'adresse IP de la passerelle
-sudo sed -i "s/^NTP=.*/NTP='$ntp1'/" /etc/systemd/timesyncd.conf
+sudo sed -i "s/^NTP=.*/NTP=$ntp1/" /etc/systemd/timesyncd.conf
 echo "Configuration de /etc/systemd/timesyncd.conf avec l'adresse IP $ntp1 (r-v66.reseau.jussieu.fr) effectuée."
 cat /etc/systemd/timesyncd.conf | grep NTP
 
