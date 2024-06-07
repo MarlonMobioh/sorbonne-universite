9b9df04ef90c2621a10aac755f2d700f9da8da94
MarlonMobioh <166839977+MarlonMobioh@users.noreply.github.com>
2024-06-07 14:05:28 +0200

    Update install_redhat8-9_prod_mmo.sh

--- install_redhat8-9_prod_mmo.sh
+++ install_redhat8-9_prod_mmo.sh
@@ -244,6 +244,7 @@ dnf install -y postfix
 systemctl stop postfix
 systemctl disable postfix
 dnf install -y shellcheck
+dnf install -y fail2ban
 dnf install -y net-tools
 dnf install -y psmisc
 dnf install -y mailx mailutils s-nail
