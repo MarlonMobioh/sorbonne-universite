ce7088e164b996a967b28d7fd647865faddf35e1
MarlonMobioh <166839977+MarlonMobioh@users.noreply.github.com>
2024-06-14 16:25:02 +0200

    Update install_redhat8-9_prod_mmo.sh

--- install_redhat8-9_prod_mmo.sh
+++ install_redhat8-9_prod_mmo.sh
@@ -201,13 +201,15 @@ echo "server 134.157.23.254 iburst" >> /etc/chrony.conf
 systemctl restart chronyd
 # Vérifier le statut du service chronyd
 systemctl status chronyd
+sleep 2
+
 echo -e "\e[92m*** Le service chronyd a été redémarré.***\e[0m"
 echo "*** Waiting 5 sec ... ***"
 sleep 5
 # Vérifier la synchronisation de l'horloge
 timedatectl
 sleep 3
-echo "\e[92mConfiguration de chronyd avec l'adresse IP 134.157.23.254 (r-v46.reseau.jussieu.fr) effectuée.\e[0m"
+echo -e "\e[92mConfiguration de chronyd avec l'adresse IP 134.157.23.254 (r-v46.reseau.jussieu.fr) effectuée.\e[0m"
 
 # Enregistrement dans RedHat (mmobioh ; Sorbonne@2023)
 echo -e "\e[94mEnregistrement dans RedHat :\e[0m"
@@ -215,11 +217,11 @@ echo "Nettoyage des informations d'inscription précédentes"
 subscription-manager clean
 echo "Enregistrement de l'utilisateur [mmobioh]"
 subscription-manager register --username=mmobioh --password=Sorbonne@2023
-echo "Affichage de la liste des abonnements disponibles"
-subscription-manager list --available
+#echo "Affichage de la liste des abonnements disponibles"
+#subscription-manager list --available
 echo "Attachement de l'abonnement spécifique identifié par le code de pool 8a85f99977b0c0420177f2a086211111s"
 subscription-manager attach --pool=8a85f99977b0c0420177f2a086211111
-echo "\e[94m*** Abonnement Redhat 8a85f99977b0c0420177f2a086211111 attaché ***\e[92m"
+echo -e "\e[94m*** Abonnement Redhat 8a85f99977b0c0420177f2a086211111 attaché ***\e[92m"
 sleep 3
 
 # Mise à jour des paquets [dnf update]
