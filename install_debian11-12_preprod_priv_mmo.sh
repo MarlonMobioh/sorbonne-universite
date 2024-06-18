904ee1d30b2e738c95d99eab85542a10562b8008
MarlonMobioh <166839977+MarlonMobioh@users.noreply.github.com>
2024-06-18 12:25:13 +0200

    Update install_debian11-12_preprod_priv_mmo.sh

--- install_debian11-12_preprod_priv_mmo.sh
+++ install_debian11-12_preprod_priv_mmo.sh
@@ -435,6 +435,12 @@ rm -f /etc/ssh/ssh_host_* && dpkg-reconfigure openssh-server && \
 # Suppression history
 history -c
 
+# Nom du script
+script_name=$(basename "$0")
+# Ajouter un fichier de log pour indiquer que le script a été exécuté
+log_file="/var/log/mmo.log"
+echo "[$(date '+%Y-%m-%d %H:%M:%S')] Le script $script_name été executé sur la machine : $new_hostname par l'utilisateur, $(whoami)" >> "$log_file"
+sleep 3
 # Message de fin de script
 echo -e "\e[94m********** Fin du script **********\e[0m"
 sleep 2
